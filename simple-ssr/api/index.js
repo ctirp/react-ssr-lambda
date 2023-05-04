de9ce82ff974d02e633d5998dc3d95b3d8f48d43
yongzheng <yongzheng@trip.com>
2023-05-04 02:50:53 +0000

    make responseBodyMock dynamic

--- simple-ssr/api/index.js
+++ simple-ssr/api/index.js
@@ -3,7 +3,7 @@
 let responseBodyMock=require("./mock");
 exports.handler = async (event) => {
   const responseCode = 200;
-  const responseBody = responseBodyMock||[
+  const responseBody = responseBodyMock(300)||[
     { id: 1, name: "item 1", desc: "product 1 description by zy", price: "1.00" },
     { id: 2, name: "item 2", desc: "product 2 description", price: "2.00" },
     { id: 3, name: "item 3", desc: "product 3 description", price: "3.00" },
--- simple-ssr/api/mock.js
+++ simple-ssr/api/mock.js
@@ -1,17 +1,23 @@
- const dataLen=500;
- let responseBody=[],responseBodyFixed = [
-    { id: -1, name: "item 1", desc: "product -1 us-east-1 description by zy", price: "1.00" },
-  ];
-    
-for (var i = dataLen; i--; ) {
-    responseBody[i]={
-        id: i, 
-        name: "flight "+i, 
-        desc: "flight "+i+" us-east-1 description:"+Math.random(), 
-        price: i+".00"
-    };
-    
+module.exports = function responseBodyMock(len) {
+    let dataLen = 500;
+    if (len) {
+        dataLen = len;
+    }
+
+    let responseBody = [],
+        responseBodyFixed = [
+            { id: -1, name: "item 1", desc: "product -1 us-east-1 description by zy", price: "1.00" },
+        ];
+
+    for (var i = dataLen; i--;) {
+        responseBody[i] = {
+            id: i,
+            name: "flight " + i,
+            desc: "flight " + i + " us-east-1 description:" + Math.random(),
+            price: i + ".00"
+        };
+
+    }
+
+    return responseBodyFixed.concat(responseBody);
 }
-    
-  
-  module.exports=responseBodyFixed.concat(responseBody);
