a46bd481f617233fdf404fcbdcccf528dd2512c5
yongzheng <yongzheng@trip.com>
2023-05-08 04:05:24 +0000

    add optional chain for products

--- simple-ssr/src/components/ProductList.js
+++ simple-ssr/src/components/ProductList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Product from "./Product";
 
 const ProductList = ({ result }) => {
-  console.log("[zy-us]ProductList",result.products.length);
+  console.log("[zy-us]ProductList",result.products?.length);
   if (result.loading) {
     return <div>loading...</div>;
   } else {
