74c6cc995dbd37c7bffd242d403bb5cbf623a93a
yongzheng <yongzheng@trip.com>
2023-05-08 04:20:34 +0000

    remove region for cdk

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -7,7 +7,8 @@ import { App } from "aws-cdk-lib";
 import { ApiStack } from "../lib/api-stack";
 import { SsrStack } from "../lib/srr-stack";
 
-const demoEnv = { region: "us-east-1" };
+// const demoEnv = { region: "us-east-1" };
+const demoEnv = { };
 const app = new App();
 new ApiStack(app, "SSRApiStack", { env: demoEnv });
 new SsrStack(app, "SSRAppStack", { env: demoEnv });
