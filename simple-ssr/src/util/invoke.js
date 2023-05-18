6de09863bfba8da50d49fef77631ae80ed916468
yongzheng <yongzheng@trip.com>
2023-05-18 18:28:48 +0800

    make invoke result toString

--- simple-ssr/src/util/invoke.js
+++ simple-ssr/src/util/invoke.js
@@ -1,8 +1,24 @@
 import { InvokeCommand, LambdaClient, LogType } from "@aws-sdk/client-lambda";
 import { createClientForDefaultRegion } from "./create-client";
 
+  const invoke = async (funcName, payload) => {
+    let start=new Date(); 
+    const client = createClientForDefaultRegion(LambdaClient);
+    const command = new InvokeCommand({
+      FunctionName: funcName,
+      Payload: JSON.stringify(payload),
+      LogType: LogType.Tail,
+    });
+  
+    const { Payload, LogResult } = await client.send(command);
+    const result = Buffer.from(Payload).toString();
+    const logs = Buffer.from(LogResult, "base64").toString();
+    console.log("[zy]invoke2 end,","invokeWaste=",new Date()-start);
+    console.log("[zy]invoke end,","result=",result,"logs=",logs);
+    return { logs, result };
+  };
 
-const invoke = async (funcName, payload) => {
+  const invoke2 = async (funcName, payload) => {
     let start=new Date();
     const client = createClientForDefaultRegion(LambdaClient);
     const command = new InvokeCommand({
@@ -18,19 +34,4 @@ const invoke = async (funcName, payload) => {
     console.log("[zy]invoke end,","result=",result,"logs=",logs);
     return { logs, result:result.data };
   };
-
-  const invoke2 = async (funcName, payload) => {
-    const client = createClientForDefaultRegion(LambdaClient);
-    const command = new InvokeCommand({
-      FunctionName: funcName,
-      Payload: JSON.stringify(payload),
-      LogType: LogType.Tail,
-    });
-  
-    const { Payload, LogResult } = await client.send(command);
-    const result = Buffer.from(Payload).toString();
-    const logs = Buffer.from(LogResult, "base64").toString();
-    return { logs, result };
-  };
-
 export {invoke,invoke2};
