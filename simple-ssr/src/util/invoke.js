8fba3f0011cfb565b01c786f179c0d72a6aa2449
yongzheng <yongzheng@trip.com>
2023-05-18 21:18:06 +0800

    implement invokeCore

--- simple-ssr/src/util/invoke.js
+++ simple-ssr/src/util/invoke.js
@@ -1,21 +1,47 @@
 import { InvokeCommand, LambdaClient, LogType } from "@aws-sdk/client-lambda";
 import { createClientForDefaultRegion } from "./create-client";
 
-  const invoke = async (funcName, payload) => {
-    let start=new Date(); 
-    const client = createClientForDefaultRegion(LambdaClient);
-    const command = new InvokeCommand({
-      FunctionName: funcName,
-      Payload: JSON.stringify(payload),
-      LogType: LogType.Tail,
-    });
-  
-    const { Payload, LogResult } = await client.send(command);
-    const result = Buffer.from(Payload).toString();
-    const logs = Buffer.from(LogResult, "base64").toString();
-    console.log("[zy]invoke end,","invokeWaste=",new Date()-start);
-    console.log("[zy]invoke end,","result=",result,"logs=",logs);
+  const invokeCore = async (funcName, payload) => {
+    let start=new Date();
+    let result = "",logs = ""; 
+    try {
+      const client = createClientForDefaultRegion(LambdaClient);
+      const command = new InvokeCommand({
+        FunctionName: funcName,
+        Payload: JSON.stringify(payload),
+        LogType: LogType.Tail,
+      });
+    
+      const { Payload, LogResult } = await client.send(command);
+      result = Buffer.from(Payload).toString();
+      logs = Buffer.from(LogResult, "base64").toString();
+      console.log("[zy]invokeCore end,","invokeCoreWaste=",new Date()-start,"str result=",result.substring(0,100));
+      // console.log("[zy]invoke end,","logs=",logs);
+    } catch (error) {
+      console.log("[zy]invokeCore error,","error=",error);
+    }
     return { logs, result };
+  };
+  const invoke = async (funcName, payload) => {
+    let start=new Date();
+    return invokeCore(funcName, payload).then((res)=>{
+      let data;
+      try {
+        let result=JSON.parse(res?.result||"{}");
+        if(result.statusCode==200){
+          data=result?.body;
+          if(typeof data==="string"){
+              data=JSON.parse(data);
+          }
+        }else{
+          console.log("[zy]invoke error,","statusCode=",result.statusCode);
+        }
+      } catch (error) {
+        console.log("[zy]invoke error,","error=",error);
+      }
+      console.log("[zy]invoke end,","invokeWaste=",new Date()-start,"data type=",typeof data);
+      return data;
+    });
   };
 
   const invoke2 = async (funcName, payload) => {
@@ -34,4 +60,4 @@ import { createClientForDefaultRegion } from "./create-client";
     console.log("[zy]invoke2 end,","result=",result,"logs=",logs);
     return { logs, result:result.data };
   };
-export {invoke,invoke2};
+export {invoke,invoke2,invokeCore};
