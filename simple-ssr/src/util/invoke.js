f2510477500842caf0ca702e7a98a22b5d1ade43
yongzheng <yongzheng@trip.com>
2023-05-18 18:01:03 +0800

    make invoke use toJSON default

--- simple-ssr/src/util/invoke.js
+++ simple-ssr/src/util/invoke.js
@@ -3,6 +3,7 @@ import { createClientForDefaultRegion } from "./create-client";
 
 
 const invoke = async (funcName, payload) => {
+    let start=new Date();
     const client = createClientForDefaultRegion(LambdaClient);
     const command = new InvokeCommand({
       FunctionName: funcName,
@@ -11,9 +12,11 @@ const invoke = async (funcName, payload) => {
     });
   
     const { Payload, LogResult } = await client.send(command);
-    const result = Buffer.from(Payload).toString();
+    const result = Buffer.from(Payload).toJSON();//result={ type: 'Buffer', data: [ 1, 2, 3, 4, 5 ] }
     const logs = Buffer.from(LogResult, "base64").toString();
-    return { logs, result };
+    console.log("[zy]invoke end,","invokeWaste=",new Date()-start);
+    console.log("[zy]invoke end,","result=",result,"logs=",logs);
+    return { logs, result:result.data };
   };
 
 
