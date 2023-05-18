5e7ea8de1f12293f5484b1b0c37075b62206d3f3
yongzheng <yongzheng@trip.com>
2023-05-18 18:02:03 +0800

    add invoke2

--- simple-ssr/src/util/invoke.js
+++ simple-ssr/src/util/invoke.js
@@ -19,5 +19,18 @@ const invoke = async (funcName, payload) => {
     return { logs, result:result.data };
   };
 
+  const invoke2 = async (funcName, payload) => {
+    const client = createClientForDefaultRegion(LambdaClient);
+    const command = new InvokeCommand({
+      FunctionName: funcName,
+      Payload: JSON.stringify(payload),
+      LogType: LogType.Tail,
+    });
+  
+    const { Payload, LogResult } = await client.send(command);
+    const result = Buffer.from(Payload).toString();
+    const logs = Buffer.from(LogResult, "base64").toString();
+    return { logs, result };
+  };
 
-export {invoke};
+export {invoke,invoke2};
